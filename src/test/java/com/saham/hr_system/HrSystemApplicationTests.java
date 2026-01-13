13f667989686cd4c5769b08ab1a82f2c080efb39
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2026-01-13 11:12:53 +0100

    update test app

--- src/test/java/com/saham/hr_system/HrSystemApplicationTests.java
+++ src/test/java/com/saham/hr_system/HrSystemApplicationTests.java
@@ -1,11 +1,13 @@
 package com.saham.hr_system;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@AutoConfigureMockMvc(addFilters = false)
 class HrSystemApplicationTests {
 
 	@Test
