33babae34da4e71c8903a59313e1b54f463e882f
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2026-01-30 09:34:52 +0100

    add and organize employee details

--- src/main/java/com/saham/hr_system/modules/loan/dto/LoanRequestResponseDto.java
+++ src/main/java/com/saham/hr_system/modules/loan/dto/LoanRequestResponseDto.java
@@ -1,16 +1,15 @@
 package com.saham.hr_system.modules.loan.dto;
 
+import com.saham.hr_system.modules.employees.model.Employee;
 import com.saham.hr_system.modules.loan.model.LoanRequest;
 import lombok.Data;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Data
 public class LoanRequestResponseDto {
     private Long id;
     private String refNumber;
-    private String requestedBy;
     private double amount;
     private String type;
     private String motif;
@@ -18,13 +17,14 @@ public class LoanRequestResponseDto {
     private boolean isApprovedByHr;
     private boolean isApprovedByFinanceDepartment;
     private String status;
+    private EmployeeDetails employeeDetails;
 
     public LoanRequestResponseDto(
             LoanRequest loanRequest
     ){
         this.id = loanRequest.getRequestId();
         this.refNumber = loanRequest.getReferenceNumber();
-        this.requestedBy = loanRequest.getEmployee().getFullName();
+        this.employeeDetails = new EmployeeDetails(loanRequest.getEmployee()); // extract employee details from the loan request
         this.amount = loanRequest.getAmount();
         this.type = loanRequest.getType().toString();
         this.motif = loanRequest.getMotif();
@@ -32,5 +32,20 @@ public class LoanRequestResponseDto {
         this.isApprovedByFinanceDepartment = loanRequest.isApprovedByFinanceDepartment();
         this.status = loanRequest.getStatus().toString();
         this.issueDate = loanRequest.getIssueDate();
+    }
+}
+
+@Data
+class EmployeeDetails{
+    private String employeeName;
+    private String employeeMatriculation;
+    private String occupation;
+    private String entity;
+
+    public EmployeeDetails(Employee employee){
+        this.employeeName = employee.getFullName();
+        this.employeeMatriculation = employee.getMatriculation();
+        this.occupation = employee.getOccupation();
+        this.entity = employee.getEntity();
     }
 }
