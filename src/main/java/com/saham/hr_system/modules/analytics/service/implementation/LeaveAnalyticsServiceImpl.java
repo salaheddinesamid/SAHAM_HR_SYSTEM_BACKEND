727916f5431902368515bffae95fa1377e8b90d3
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2026-02-19 11:13:53 +0000

    init leave analytics

--- src/main/java/com/saham/hr_system/modules/analytics/service/implementation/LeaveAnalyticsServiceImpl.java
+++ src/main/java/com/saham/hr_system/modules/analytics/service/implementation/LeaveAnalyticsServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -25,8 +26,57 @@ public class LeaveAnalyticsServiceImpl implements LeaveAnalyticsService {
 
     @Override
     public LeaveAnalyticsDto getLeaveAnalyticsOverview(String type, LocalDate from, LocalDate to, String department, String entity) {
+        // Leaves
         List<Leave> leaves = leaveRepository.findAll();
-        /* TODO: Implement the logic to filter the leaves based on the provided parameters (type, from, to, department, entity) and calculate the analytics data. */
+        List<Leave> filteredLeaves = new ArrayList<>();
+        // Leave Requests
+        List<LeaveRequest> leaveRequests = leaveRequestRepository.findAll();
+
+        List<LeaveRequest> filteredLeaveRequests = new ArrayList<>();
+        // If the type is ALL, we don't filter by type, otherwise we filter by the specified type
+        if(type.equals("ALL")){
+            filteredLeaves = leaves;
+        }
+        if(!type.equals("ALL")){
+            filteredLeaves = filterByType(leaves,type);
+        }
+        // If the from and to dates are provided, we filter by the date range
+        if(from != null && to != null){
+            filteredLeaves = filterLeavesByDateRange(filteredLeaves, from, to);
+        }
+        // If the department is not ALL, we filter by the specified department
+        if(!department.equals("ALL")){
+            filteredLeaves = filterLeavesByDepartment(filteredLeaves, department);
+        }
+        // If the entity is not ALL, we filter by the specified entity
+        if(!entity.equals("ALL")){
+            filteredLeaves = filterLeavesByEntity(filteredLeaves, entity);
+        }
+
+        long totalLeaves = filteredLeaves.size();
         return null;
+    }
+
+    private List<Leave> filterByType(List<Leave> leaves, String type) {
+        return leaves.stream()
+                .filter(leave -> leave.getLeaveType().toString().equals(type))
+                .toList();
+    }
+    private List<Leave> filterLeavesByDateRange(List<Leave> leaves, LocalDate from, LocalDate to) {
+        return leaves.stream()
+                .filter(leave -> (leave.getFromDate().isEqual(from) || leave.getToDate().isAfter(from)) &&
+                        (leave.getToDate().isEqual(to) || leave.getFromDate().isBefore(to)))
+                .toList();
+    }
+
+    private List<Leave> filterLeavesByDepartment(List<Leave> leaves, String department) {
+        return leaves.stream()
+                .filter(leave -> leave.getEmployee().getEmployeeProfessionalDetails().getDepartment().toString().equals(department))
+                .toList();
+    }
+    private List<Leave> filterLeavesByEntity(List<Leave> leaves, String entity) {
+        return leaves.stream()
+                .filter(leave -> leave.getEmployee().getEmployeeProfessionalDetails().getEntity().toString().equals(entity))
+                .toList();
     }
 }
