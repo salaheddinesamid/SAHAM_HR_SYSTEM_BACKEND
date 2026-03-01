4f5cd5b3c32a32de09842ab5f4505dc9df40e9b1
salaheddinesamid <143968052+salaheddinesamid@users.noreply.github.com>
2026-03-01 22:41:01 +0000

    update CORS config

--- src/main/java/com/saham/hr_system/config/SecurityConfiguration.java
+++ src/main/java/com/saham/hr_system/config/SecurityConfiguration.java
@@ -124,9 +124,9 @@ public class SecurityConfiguration {
                 registry.addMapping("/**")
                         .allowedOrigins(
                                 "http://localhost:3000",
-                                "http://192.168.15.27:3000",
+                                "http://192.168.15.27:3000/",
                                 "http://localhost:3001",
-                                "http://192.168.15.27:3001",
+                                "http://192.168.15.27:3001/",
                                 "http://hr.saham.local"
                         )
                         .allowedMethods("*")
